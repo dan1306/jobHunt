ca927923df1e2ee1411ebea78f16b0b42142ffb1
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-04 11:20:41 -0400

    first

---
+++ src/pages/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Hi
+          </h1>
+        </header>
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
