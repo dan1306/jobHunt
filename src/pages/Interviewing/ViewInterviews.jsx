4d05170fb7a70c1456a321757cab28200298618b
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-11 17:25:32 -0400

    interviewing view css

--- src/pages/Interviewing/ViewInterviews.jsx
+++ src/pages/Interviewing/ViewInterviews.jsx
@@ -1,40 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Interviewing.css";
 
 class Interviewing extends Component {
-
   state = {
-    interviewing: []
-}
+    interviewing: [],
+  };
 
-  
   async componentDidMount() {
-    let getInterviews = await fetch(`/api/interviewing/getInterviews/${this.props.huntId}`)
-    getInterviews = await getInterviews.json()
-    this.setState({ interviewing: getInterviews })
-    console.log(this.state)
+    let getInterviews = await fetch(
+      `/api/interviewing/getInterviews/${this.props.huntId}`
+    );
+    getInterviews = await getInterviews.json();
+    this.setState({ interviewing: getInterviews });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="intervContainer">
+        {this.state.interviewing.map((val, id) => {
+          return (
+            <div className="interDiv">
+              <h2>Company: {val.Company}</h2>
+
+              <h3>Job title: {val.JobTitle}</h3>
+              <br />
+
+              <p>Interview Round: {val.RoundOfInterview}</p>
+
+              <p>Interview Date: {val.InterviewDate}</p>
+              <div className="moveRight">
+                <button
+                  onClick={this.handleSubmit}
+                  type="submit"
+                  class="btn btn-primary spaceOut"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={this.handleSubmit}
+                  type="submit"
+                  class="btn btn-danger spaceOut"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-    render() {
-      return (
-        <>
-          <h1>hiasdad</h1>
-        {
-            this.state.interviewing.map((val, id) => {
-                return (
-                    <div className='interDiv'>
-                        
-                        {val.JobTitle}
-                        <br/>
-                        {val.Company}
-                        <br/>
-                        {val.InterviewDate}
-                    </div>
-                )
-            })
-        }
-
-  </>      );
-    }
-  }
-  
-  export default Interviewing;
+export default Interviewing;
