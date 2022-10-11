32c07337e4494c40e1056de0199f570d34c59ceb
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-11 16:52:11 -0400

    view offers css

--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -1,40 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Offer extends Component {
+  state = {
+    offers: [],
+  };
 
-    state = {
-        offers: []
-    }
-
-    async componentDidMount() {
-        let getOffers = await fetch(`/api/offer/getOffers/${this.props.huntId}`)
-        getOffers = await getOffers.json()
-        this.setState({ offers: getOffers })
-        console.log(this.state)
-    }
-
-
-    render() {
-      return (
-          <>
-              {
-                  this.state.offers.map((val, id) => {
-                      return (
-                          <div className='offerDiv'>
-                              
-                              {val.JobTitle}
-                              <br/>
-                              {val.PayPerYear}
-                              <br/>
-                              {val.starDate}
-                          </div>
-                      )
-                  })
-              }
-
-        </>
-      );
-    }
+  async componentDidMount() {
+    let getOffers = await fetch(`/api/offer/getOffers/${this.props.huntId}`);
+    getOffers = await getOffers.json();
+    this.setState({ offers: getOffers });
+    console.log(this.state);
   }
-  
-  export default Offer;
+
+  render() {
+    return (
+      <div className="offerContainer">
+        {this.state.offers.map((val, id) => {
+          return (
+            <div className="offerDiv">
+              <h1>Job Title: {val.JobTitle}</h1>
+              <br />
+              <h3>Pay Per Year: ${val.PayPerYear}</h3>
+              <br />
+              <p>Start Date: {val.starDate}</p>
+              <div className="moveRight">
+                <button
+                  onClick={this.handleSubmit}
+                  type="submit"
+                  class="btn btn-success spaceOut"
+                >
+                  Accept
+                </button>
+                <button
+                  onClick={this.handleSubmit}
+                  type="submit"
+                  class="btn btn-primary spaceOut"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={this.handleSubmit}
+                  type="submit"
+                  class="btn btn-danger spaceOut"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Offer;
