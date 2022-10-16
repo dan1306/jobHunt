2123c81bf8325b1811f3404640342ac1c8a8239a
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-16 10:31:15 -0400

    deploy

--- src/components/Offer/ViewOffers.jsx
+++ src/components/Offer/ViewOffers.jsx
@@ -17,6 +17,26 @@ class Offer extends Component {
     offerAcc: null,
   };
 
+  // async componentDidMount() {
+  //   let getOffers = await fetch(`/api/offer/getOffers/${this.props.huntId}`);
+  //   getOffers = await getOffers.json();
+  //   getOffers.sort((a, b) => {
+  //     return a.PayPerYear - b.PayPerYear;
+  //   });
+  //   await this.setState({ offers: getOffers, error: "" });
+  //   console.log(this.state);
+
+  //   for (let i = 0; i < getOffers.length; i++) {
+  //     if (getOffers[i]["Accepted"]) {
+  //       await this.setState({
+  //         acceptedOffer: true,
+  //         offerAcc: "An Offer Has Been Accepted",
+  //       });
+  //       break;
+  //     }
+  //   }
+  // }
+
   async componentDidMount() {
     let getOffers = await fetch(`/api/offer/getOffers/${this.props.huntId}`);
     getOffers = await getOffers.json();
@@ -26,8 +46,8 @@ class Offer extends Component {
     await this.setState({ offers: getOffers, error: "" });
     console.log(this.state);
 
-    for (let i = 0; i < getOffers.length; i++) {
-      if (getOffers[i]["Accepted"]) {
+    for (let i = 0; i < this.state.offers.length; i++) {
+      if (this.state.offers[i]["Accepted"]) {
         await this.setState({
           acceptedOffer: true,
           offerAcc: "An Offer Has Been Accepted",
